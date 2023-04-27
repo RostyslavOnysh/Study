7baf225fc50e9436f4ba1302d07f8c635780cc95
Ros <rospsix@gmail.com>
2023-04-27 12:05:24 +0100

    solved task using ternary operator

---
+++ src/MateAcademy/FootballMatch/Fotball.java
@@ -0,0 +1,8 @@
+package MateAcademy.FootballMatch;
+
+public class Fotball {
+    public int matchResult(int result1, int result2, int bet1, int bet2) {
+        return result1 == bet1 && result2 == bet2 ? 2 :
+                (result1 > result2 && bet1 > bet2 || result1 < result2 && bet1 <         bet2 || result1 == result2 && bet1 == bet2) ? 1 : 0;
+    }
+}
