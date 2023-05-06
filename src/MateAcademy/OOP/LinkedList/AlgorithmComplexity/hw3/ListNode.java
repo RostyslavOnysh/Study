fb90c6b4e8b4593d07ff1113618fe2bb30daafe3
Ros <rospsix@gmail.com>
2023-05-06 11:01:40 +0100

    new solved task

---
+++ src/MateAcademy/OOP/LinkedList/AlgorithmComplexity/hw3/ListNode.java
@@ -0,0 +1,11 @@
+package MateAcademy.OOP.LinkedList.AlgorithmComplexity.hw3;
+
+public class ListNode {
+    int value;
+    ListNode prev;
+    ListNode next;
+
+    ListNode(int x) {
+        value = x;
+    }
+}
