0a727e600f8da65deacf1eaab2c0afaeac54d7e2
Ros <rospsix@gmail.com>
2023-05-21 15:14:30 +0100

    #4 add test

--- src/MateAcademy/LeetCode/TopInterwievQuestions/RemoveDuplicats.java
+++ src/MateAcademy/LeetCode/TopInterwievQuestions/RemoveDuplicats.java
@@ -1,7 +1,34 @@
 package MateAcademy.LeetCode.TopInterwievQuestions;
 
 public class RemoveDuplicats {
-    public int removeDuplicatsFromArray(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 1, 2};
+        int[] expected1 = {1, 2};
+        test(nums1, expected1);
+    }
+
+    public static void test(int[] nums, int[] expected) {
+        int k = removeDuplicatsFromArray(nums);
+        System.out.println("Number of unique element`s : " + k);
+        if (k == expected.length) {
+            boolean passed = true;
+            for (int i = 0; i < k; i++) {
+                if (nums[i] != expected[i]) {
+                    passed = false;
+                    break;
+                }
+            }
+            if (passed) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        } else {
+            System.out.println("Test failed");
+        }
+    }
+
+    public static int removeDuplicatsFromArray(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
